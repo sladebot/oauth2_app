52da0964d19224d97cb590b927d8c999a70a7282
Souranil  Sen <souranil.sen@tcs.com>
2013-05-14 18:22:31 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oauth2App::Application.config.secret_token = '85b4699fa7a76ce0c776e6f46901a6b86f325a7e70c69c76765d9b1992a2bee74d0e685e525805babf4cd61c706fe4e06c5bd039db128a78c530902b39a2a196'
